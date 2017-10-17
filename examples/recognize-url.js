134744b5b3bc718ae19404f46c1559c31904931d
Oliver Iusztin <oliver.iusztin@uni-duesseldorf.de>
2017-10-17 16:02:20 +0200

    basic recognition class, basic promises

--- examples/recognize-url.js
+++ examples/recognize-url.js
@@ -7,4 +7,12 @@ const key = require('./secret-key');
 // example code
 let myClient = new scnnr.Client({ key });
 
-myClient.recognizeUrl("http://tokyofashion.com/wp-content/uploads/2017/10/TK-2017-09-09-016-001-Harajuku.jpg");
+myClient.recognizeUrl("http://tokyofashion.com/wp-content/uploads/2017/10/TK-2017-09-09-016-001-Harajuku.jpg")
+.then(result => {
+    let prettyResult = JSON.stringify(result, null, 2);
+    console.log(prettyResult);
+})
+.catch(error => {
+    console.log(error);
+})
+
