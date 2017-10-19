b90cc7eebbd5e2203d86e537560b315c2f6c4d9e
Oliver Iusztin <oliver.iusztin@uni-duesseldorf.de>
2017-10-19 15:24:02 +0200

    created pending tests

--- test/client.js
+++ test/client.js
@@ -1,35 +1,33 @@
 "use strict";
-const scnnr = require('../lib/scnnr');
 const chai = require('chai');
+const nock = require('nock')
+
+const scnnr = require('../lib/scnnr');
+const queuedRecognition = require('./fixtures/queued_recognition');
+
 const should = chai.should();
 
-/*
-describe('Client', () => {
+describe('Client', () => { 
     const config = {
         url: 'https://dummy.scnnr.cubki.jp/',
         version: 'v1',
-        timeout: 0,
         key: 'dummy_key',
     };
 
     let testClient;
-
+    
     beforeEach((done) => {
         testClient = new scnnr.Client(config);
         done();
     });
-    
+
     describe('recognizeUrl', () => {
+        let dummyServer;
         const url = 'https://example.com/dummy.jpg';
+        // dummyServer = nock('https://dummy.scnnr.cubki.jp').post('/v1/', { url }).reply(200, queuedRecognition);
 
-        it('should be queued', (done) => {
-            testClient.recognizeUrl(url)
-            .then(result => {
-                /// do testing
-                done();
-            })
-        });
-
+        it('should send url in post body');
+        it('should resolve with queued recognition')
     });
+
 });
-*/
