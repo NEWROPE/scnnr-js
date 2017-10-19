1c82ed223629caf4afb78e36678e15ef940de1df
Oliver Iusztin <oliver.iusztin@uni-duesseldorf.de>
2017-10-19 15:31:32 +0200

    made connection json body test simpler

--- test/connection.js
+++ test/connection.js
@@ -3,7 +3,6 @@ const chai = require('chai');
 const nock = require('nock')
 
 const Connection = require('../lib/scnnr/client/connection');
-const queuedRecognition = require('./fixtures/queued_recognition');
 
 const should = chai.should();
 
@@ -13,14 +12,15 @@ describe('Connection', () => {
         version: 'v1',
         key: 'dummy_key',
     };
-
-    const dummyServer = nock('https://dummy.scnnr.cubki.jp').post('/v1/').reply(200, queuedRecognition);
     const testConnection = new Connection(config);
 
     it('resolves with json body', () => {
-        return testConnection.sendJson('/', { data: 'dummy_data' } )
+        const testData = { data: 'dummy_data' };
+        const dummyServer = nock(config.url).post('/v1/', testData).reply(200, testData);
+        
+        return testConnection.sendJson('/', testData)
         .then( result => {
-            result.should.eql(queuedRecognition);
+            result.should.eql(testData);
         });
     });
 
