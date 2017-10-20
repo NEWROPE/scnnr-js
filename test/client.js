bd66782a4142d04a79bd4b231cd972a715db2142
Oliver Iusztin <oliver.iusztin@uni-duesseldorf.de>
2017-10-20 06:30:04 +0200

    added eslint config and reformatted the code

--- test/client.js
+++ test/client.js
@@ -1,38 +1,39 @@
-"use strict";
-const chai = require('chai');
-const nock = require('nock');
+'use strict'
 
-const scnnr = require('../lib/scnnr');
-const queuedRecognition = require('./fixtures/queued_recognition');
+const chai = require('chai')
+const nock = require('nock')
 
-const should = chai.should();
+const scnnr = require('../lib/scnnr')
+const queuedRecognition = require('./fixtures/queued_recognition')
+
+const should = chai.should()
 
 describe('Client', () => { 
-    const config = {
-        url: 'https://dummy.scnnr.cubki.jp/',
-        version: '',
-        key: '',
-    };
-    const testClient = new scnnr.Client(config);
+  const config = {
+    url: 'https://dummy.scnnr.cubki.jp/',
+    version: '',
+    key: '',
+  }
+  const testClient = new scnnr.Client(config)
 
-    describe('recognizeUrl', () => {
-        const path = '/remote/recognitions';
+  describe('recognizeUrl', () => {
+    const path = '/remote/recognitions'
 
-        it('should send url in post body', () => {
-            const url = 'https://example.com/dummy.jpg';
-            nock(config.url).post(path, { url }).reply(200);
+    it('should send url in post body', () => {
+      const url = 'https://example.com/dummy.jpg'
+      nock(config.url).post(path, { url }).reply(200)
 
-            return testClient.recognizeUrl(url);
-        });
+      return testClient.recognizeUrl(url)
+    })
 
-        it('should resolve with queued recognition', () => {
-            nock(config.url).post(path).reply(200, queuedRecognition);
+    it('should resolve with queued recognition', () => {
+      nock(config.url).post(path).reply(200, queuedRecognition)
 
-            return testClient.recognizeUrl('')
-            .then(result => {
-                result.should.be.eql(queuedRecognition);
-            });
-        });
-    });
+      return testClient.recognizeUrl('')
+        .then(result => {
+          result.should.be.eql(queuedRecognition)
+        })
+    })
+  })
 
-});
+})
--- test/connection.js
+++ test/connection.js
@@ -1,59 +1,60 @@
-"use strict";
-const chai = require('chai');
+'use strict'
+
+const chai = require('chai')
 const nock = require('nock')
 
-const Connection = require('../lib/scnnr/client/connection');
+const Connection = require('../lib/scnnr/client/connection')
 
-const should = chai.should();
+const should = chai.should()
 
 describe('Connection', () => { 
-    const config = {
-        url: 'https://dummy.scnnr.cubki.jp/',
-        version: 'v1',
-        key: 'dummy_key'
-    };
-    const testConnection = new Connection(config);
-    const versionPath = `/${config.version}/`;
+  const config = {
+    url: 'https://dummy.scnnr.cubki.jp/',
+    version: 'v1',
+    key: 'dummy_key'
+  }
+  const testConnection = new Connection(config)
+  const versionPath = `/${config.version}/`
 
-    describe('sendJson', () => {
-        const testData = { data: 'dummy_data' };
+  describe('sendJson', () => {
+    const testData = { data: 'dummy_data' }
 
-        it('sends x-api-key', () => {
-            nock(config.url, { reqheaders: { 'x-api-key': config.key } }).post(versionPath).reply(200);
+    it('sends x-api-key', () => {
+      nock(config.url, { reqheaders: { 'x-api-key': config.key } }).post(versionPath).reply(200)
 
-            return testConnection.sendJson('/', '');
-        });
+      return testConnection.sendJson('/', '')
+    })
 
-        it('sends application/json header', () => {
-            nock(config.url, { reqheaders: { 'Content-Type': 'application/json' } }).post(versionPath).reply(200);
+    it('sends application/json header', () => {
+      nock(config.url, { reqheaders: { 'Content-Type': 'application/json' } }).post(versionPath).reply(200)
             
-            return testConnection.sendJson('/', '');
-        });
+      return testConnection.sendJson('/', '')
+    })
 
-        it('sends timeout parameter', () => {
-            const timeout = { timeout: 1 };
-            const timeoutConfig = Object.assign({}, config, timeout);
-            const timeoutConnection = new Connection(timeoutConfig);
+    it('sends timeout parameter', () => {
+      const timeout = { timeout: 1 }
+      const timeoutConfig = Object.assign({}, config, timeout)
+      const timeoutConnection = new Connection(timeoutConfig)
 
-            nock(config.url).post(versionPath).query(timeout).reply(200);
+      nock(config.url).post(versionPath).query(timeout).reply(200)
 
-            return timeoutConnection.sendJson('/', '');
-        });
+      return timeoutConnection.sendJson('/', '')
+    })
 
-        it('sends json body', () => {
-            nock(config.url).post(versionPath, testData).reply(200);
+    it('sends json body', () => {
+      nock(config.url).post(versionPath, testData).reply(200)
             
-            return testConnection.sendJson('/', testData);
-        });
+      return testConnection.sendJson('/', testData)
+    })
 
-        it('resolves with json body', () => {
-            nock(config.url).post(versionPath).reply(200, testData);
+    it('resolves with json body', () => {
+      nock(config.url).post(versionPath).reply(200, testData)
             
-            return testConnection.sendJson('/', '')
-            .then( result => {
-                result.should.eql(testData);
-            });
-        });
-    });
+      return testConnection.sendJson('/', '')
+        .then( result => {
+          result.should.eql(testData)
+        })
+    })
+  })
 
-});
+})
